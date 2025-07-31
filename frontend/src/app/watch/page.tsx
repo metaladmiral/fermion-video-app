a404338493a6afd33ddac17c20d35818fee330fa
Metaladmiral <thesecretinspiration@gmail.com>
2025-07-31 09:02:47 +0530

    fix: m3u8 url

--- frontend/src/app/watch/page.tsx
+++ frontend/src/app/watch/page.tsx
@@ -8,13 +8,9 @@ export default function WatchPage() {
   //   const m3u8Source = "http://localhost:3001/hls/stream.m3u8";
   //   const m3u8Source = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
   useEffect(() => {
-    let host = "localhost";
-    let port = "3001";
-    if (typeof window !== "undefined") {
-      host = window.location.host.split(":")[0];
-    }
+    const host = process.env.NEXT_PUBLIC_BACKEND_SERVER_URL || "localhost:3001";
 
-    const m3u8Source = `http://${host}:${port}/hls/stream.m3u8`;
+    const m3u8Source = `${host}/hls/stream.m3u8`;
 
     const hlsSupported = Hls.isSupported();
     let hls: Hls;
